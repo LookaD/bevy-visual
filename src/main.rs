68627ac84c968318d12cd3dcb81e22ef38ef3e7a
LookaD <luca.db1996@gmail.com>
2023-11-25 01:59:03 +0100

    - Fixed some errors in the code
    - added ball movement and collision detection with the walls of the screen
    
    - updated ui handling

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,58 @@ struct Ball {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    // Aggiungi qui la palla e il quadrato
+    // Aggiungi la palla
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(30.0, 30.0)),
+            color: Color::rgb(1.0, 0.5, 0.5),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(Ball { speed: Vec3::new(100.0, 100.0, 0.0) });
+
+    // Aggiungi qui eventualmente altri elementi
 }
 
-fn ball_movement_system(time: Res<Time>, mut query: Query<(&Ball, &mut Transform)>) {
-    for (ball, mut transform) in query.iter_mut() {
+fn ball_movement_system(
+    mut windows: Query<&mut Window>, 
+    time: Res<Time>, 
+    mut query: Query<(&mut Ball, &mut Transform)>
+) {
+    let window = windows.single_mut();
+    let window_width = window.width() as f32;
+    let window_height = window.height() as f32;
+
+    for (mut ball, mut transform) in query.iter_mut() {
         transform.translation += ball.speed * time.delta_seconds();
 
-        // Aggiungi qui la logica per far rimbalzare la palla sulle pareti
+        // Controllo collisioni con le pareti orizzontali
+        if transform.translation.x >= window_width / 2.0 || transform.translation.x <= -window_width / 2.0 {
+            ball.speed.x = -ball.speed.x;
+        }
+
+        // Controllo collisioni con le pareti verticali
+        if transform.translation.y >= window_height / 2.0 || transform.translation.y <= -window_height / 2.0 {
+            ball.speed.y = -ball.speed.y;
+        }
     }
 }
 
 fn ui_system(mut egui_context: EguiContexts, mut query: Query<&mut Ball>) {
     egui::Window::new("Impostazioni Palla").show(egui_context.ctx_mut(), |ui| {
         for mut ball in query.iter_mut() {
-            ui.add(egui::Slider::new(&mut ball.speed.x, 0.0..=1000.0).text("Velocità X"));
-            ui.add(egui::Slider::new(&mut ball.speed.y, 0.0..=1000.0).text("Velocità Y"));
+            // Calcola il valore assoluto della velocità
+            let mut speed_x_abs = ball.speed.x.abs();
+            let mut speed_y_abs = ball.speed.y.abs();
+
+            // Mostra gli slider per i valori assoluti della velocità
+            ui.add(egui::Slider::new(&mut speed_x_abs, 0.0..=1000.0).text("Velocità X"));
+            ui.add(egui::Slider::new(&mut speed_y_abs, 0.0..=1000.0).text("Velocità Y"));
+
+            // Aggiorna la velocità mantenendo il segno originale
+            ball.speed.x = ball.speed.x.signum() * speed_x_abs;
+            ball.speed.y = ball.speed.y.signum() * speed_y_abs;
         }
     });
 }
